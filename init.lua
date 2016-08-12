SmallJoker/boost_cart

commit f3bc9d795d35ae056250d74fad6db384a2d480ea

Fix undeclared global variable "mesecon"

init.lua
@@ -24,7 +24,7 @@ end
 dofile(boost_cart.modpath.."/functions.lua")
 dofile(boost_cart.modpath.."/rails.lua")
 
-if mesecon then
+if minetest.global_exists(mesecon) then
 	dofile(boost_cart.modpath.."/detector.lua")
 end
 

commit 7d6132e748740c9191ca24be520ccb778f6e6644

Fix mesecon variable bug caused by f3bc9d7

init.lua
@@ -24,7 +24,8 @@ end
 dofile(boost_cart.modpath.."/functions.lua")
 dofile(boost_cart.modpath.."/rails.lua")
 
-if minetest.global_exists(mesecon) then
+local HAVE_MESECONS_ENABLED = minetest.global_exists("mesecon")
+if HAVE_MESECONS_ENABLED then
 	dofile(boost_cart.modpath.."/detector.lua")
 end
 
@@ -279,7 +280,7 @@ function boost_cart.cart:on_step(dtime)
 		new_acc = vector.multiply(dir, acc)
 	end
 
-	if mesecon then
+	if HAVE_MESECONS_ENABLED then
 		boost_cart:signal_detector_rail(vector.round(pos))
 	end
 